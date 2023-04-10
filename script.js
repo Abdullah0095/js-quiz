08c3846867d2a495de614084a5994c6e3c4cc509
Abdullah0095 <abdullah.web1995@gmail.com>
2023-04-11 03:20:59 +0600

    new question added

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
  const questions = [
     {
+        question : "What does HTML stand for?",
+        answers : [
+            {text: "Hyper Text Markup Language ", correct: true},
+            {text: "Home Tool Markup Language", correct: false},
+            {text: "Hyperlinks and Text Markup Language", correct: false},
+            {text: "Hyper Text Makeup Language ", correct: false}
+        ]
+    },
+
+    {
+        question : "Who is making the Web standards?",
+        answers : [
+            {text: "Google", correct: false},
+            {text: "Mozilla", correct: false},
+            {text: "The World Wide Web Consortium  ", correct: true},
+            {text: "Microsoft", correct: false}
+        ]
+    },
+    {
+        question : "Choose the correct HTML element for the largest heading:",
+        answers : [
+            {text: "<h1>  ", correct: true},
+            {text: "<h6>", correct: false},
+            {text: "<heading>", correct: false},
+            {text: "<head>", correct: false}
+        ]
+    },
+    {
+        question : "Choose the correct HTML element to define important text",
+        answers : [
+            {text: "<b>", correct: false},
+            {text: "<i>", correct: false},
+            {text: "<important>", correct: false},
+            {text: "<strong>", correct: true}
+        ]
+    },
+    {
+        question : "Choose the correct HTML element to define emphasized text",
+        answers : [
+            {text: "<em>  ", correct: true},
+            {text: "<italic>", correct: false},
+            {text: "<i>", correct: false},
+            {text: "<pre>", correct: false}
+        ]
+    },
+    {
+        question : "Inline elements are normally displayed without starting a new line.",
+        answers : [
+            {text: "true", correct: true},
+            {text: "false", correct: false}
+            // {text: "dampy", correct: false},
+            // {text: "mumpy", correct: false}
+        ]
+    },
+    {
         question : "what is my name?",
         answers : [
             {text: "bappy bro", correct: true},
@@ -8,36 +63,33 @@
             {text: "mumpy", correct: false}
         ]
     },
-
     {
-        question : "what is my favorite color?",
+        question : "what is my name?",
         answers : [
-            {text: "white", correct: false},
-            {text: "black", correct: true},
-            {text: "red", correct: false},
-            {text: "green", correct: false}
+            {text: "bappy bro", correct: true},
+            {text: "hampy", correct: false},
+            {text: "dampy", correct: false},
+            {text: "mumpy", correct: false}
         ]
     },
-
     {
-        question : "what is my favorite food?",
+        question : "what is my name?",
         answers : [
-            {text: "briyani", correct: false},
-            {text: "chips", correct: false},
-            {text: "banana", correct: true},
-            {text: "kabab", correct: false}
+            {text: "bappy bro", correct: true},
+            {text: "hampy", correct: false},
+            {text: "dampy", correct: false},
+            {text: "mumpy", correct: false}
         ]
     },
-
     {
-        question : "what is my favorite bike?",
+        question : "what is my name?",
         answers : [
-            {text: "mt15", correct: false},
-            {text: "ktm", correct: false},
-            {text: "gixxer", correct: true},
-            {text: "yamaha v2", correct: false}
+            {text: "bappy bro", correct: true},
+            {text: "hampy", correct: false},
+            {text: "dampy", correct: false},
+            {text: "mumpy", correct: false}
         ]
-    }
+    },
  ];
 
  const questionElement = document.getElementById("question");
